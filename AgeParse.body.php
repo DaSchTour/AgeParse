6ab09e36b3dbc8e35fc3868533c4bc95ca75a639
DaSchTour <daschtour@me.com>
2012-07-15 17:51:15 +0200

    initial commit

---
+++ AgeParse.body.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * Calculate the actual age based on information provided by the {{age}} request.
+ * Each attribute is already parsed so this function is only responsible for the 
+ * actual calculation.
+**/
+function calculateAge($from,$to,$format,$left,$right,$errbox,$zeronegatives) {
+	$retval = "";
+	$bError = true;	// error occurred in attributes; false = no error; true = error
+ 
+	// process the attributes
+	if (strlen($from) == 0) {
+		$retval = wfMsg('required-from');			// FROM is required, even if from $sToday
+	} elseif (strlen($to) == 0) {
+		$retval = wfMsg('required-to');				// TO is required, event if from $sToday
+	} elseif ($from == $to) {
+		$retval = '0 ' . wfMsg('dd-plural');		// no need to calculate anything, they are the same date
+	} elseif ($zeronegatives && $to < $from) {
+		$retval = '0 ' . wfMsg('dd-plural');		// TO comes before FROM, so we use '0 days'
+	} else {
+		$aDate = array();
+		$aDate = explode('-',$from);				// separate pages in format [<era>-]<yyyy>-<mm>-<dd>
+		$first = array();
+		$first['year']  = $aDate[0] *1;				// '*1' makes sure it is a number (makes some versions of PHP happy)
+		$first['month'] = $aDate[1] *1;
+		$first['day']   = $aDate[2] *1;
+ 
+		$aDate = explode('-',$to);					// separate pages in format [<era>-]<yyyy>-<mm>-<dd>
+		$last  = array();
+		$last['year']   = $aDate[0] *1;				// '*1' makes sure it is a number (makes some versions of PHP happy)
+		$last['month']  = $aDate[1] *1;
+		$last['day']    = $aDate[2] *1;
+ 
+		// ok, ready to calculate answer
+		$diff = date_difference($first,$last);
+		if (is_array($diff)) { 			// got something, build answer
+			$bError = false;			// turn off error indicator
+			if ($diff['years'] > 0) {	// year included in any format
+				$retval .= $diff['years'] . ' ' . ($diff['years'] == 1 ? wfMsg('yy-single') : wfMsg('yy-plural'));
+			}
+ 
+			if ($diff['months'] > 0 && ($format == wfMsg('ymd') || $format == wfMsg('ym'))) {
+				if (strlen($retval) > 0) $retval .= wfMsg('answer-sep');
+				$retval .= $diff['months'] . ' ' . ($diff['months'] == 1 ? wfMsg('mm-single') : wfMsg('mm-plural'));
+			}
+ 
+			if ($diff['days'] > 0 && ($format == wfMsg('ymd'))) {
+				if (strlen($retval) > 0) $retval .= wfMsg('answer-sep');
+				$retval .= $diff['days'] . ' ' . ($diff['days'] == 1 ? wfMsg('dd-single') : wfMsg('dd-plural'));
+			}
+		} else {
+			$retval = $diff; // some error message from date_difference()
+		}
+	}
+ 
+	// prepare answer
+	if (strlen($retval) == 0) 
+		$retval = wfMsg('catchall');
+ 
+	if (strlen($left) > 0 || strlen($right) > 0) 
+		$retval = $left . $retval . $right;
+ 
+	if ($bError && $errbox) 
+		$retval = '<div class="'.wfMsg('errorbox').'">'.$retval.'</div>';	// 'errorbox' is found in style sheet (CSS) so it is fixed (not translated)
+ 
+	return $retval;
+}
+ 
+ 
+/**
+ * Convenience function to create date in "YYYYMMDD" format without punctuation (-).
+**/
+function smoothdate ( $year, $month, $day ) {
+    return sprintf ('%04d', $year) . sprintf ('%02d', $month) . sprintf ('%02d', $day);
+}
+ 
+ 
+/**
+ * Date Difference performs the actual calculations. This function is based on a function 
+ * posted at http://www.tek-tips.com/faqs.cfm?fid=3493 on 2003-04-24 as 'faq434-3493'.
+ *
+ * == INPUT ==
+ *  $first is FROM or STARTING date
+ *  $second is TO or ENDING date
+ *  Both arguments must be an associative array as follows:
+ *     array ( 'year' => year_value, 'month' => month_value, 'day' => day_value )
+ * 
+ * == RULES ==
+ *   It does not make use of 32-bit unix timestamps, so it will work for dates
+ *   outside the range 1970-01-01 through 2038-01-19. This function works by
+ *   taking the earlier date finding the maximum number of times it can
+ *   increment the years, months, and days (in that order) before reaching
+ *   the second date. The function does take yeap years into account, but does
+ *   not take into account the 10 days removed from the calendar (specifically
+ *   October 5 through October 14, 1582) by Pope Gregory XIII to fix calendar drift.
+ *   
+ *   The first input array is the earlier date, the second the later date. It
+ *   will check to see that the two dates are well-formed, and that the first
+ *   date is earlier than the second.
+ *
+ * == OUTPUT ==
+ *   If successful, function returns an associative array as follows:
+ *      array ( 'years' => years_different, 'months' => months_different, 'days' => days_different )
+ *   
+ *   If an error occurred, function returns a string value.
+**/
+function date_difference ( $first, $second ) {
+    $month_lengths = array (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+	$retval = "";
+    if (!checkdate($first['month'], $first['day'], $first['year'])) {
+		$retval = wfMsg('invalid-from',$first['year'],$first['month'],$first['day']);
+	} elseif (!checkdate($second['month'], $second['day'], $second['year'])) {
+		$retval = wfMsg('invalid-to',$second['year'],$second['month'],$second['day']);
+	} elseif (mktime(0,0,0,$first['month'],$first['day'],$first['year']) > mktime(0,0,0,$second['month'],$second['day'],$second['year'])) {
+		$retval = wfMsg('from-first',$first['year'], $first['month'], $first['day'], $second['year'], $second['month'], $second['day']);
+	} else {
+        $start  = smoothdate ($first['year'],  $first['month'],  $first['day']);
+        $target = smoothdate ($second['year'], $second['month'], $second['day']);
+        if ($start <= $target) {
+            $add_year = 0;
+            while (smoothdate ($first['year']+ 1, $first['month'], $first['day']) <= $target) {
+                $add_year++;
+                $first['year']++;
+            } //while years
+ 
+            $add_month = 0;
+            while (smoothdate ($first['year'], $first['month'] + 1, $first['day']) <= $target) {
+                $add_month++;
+                $first['month']++;
+                if ($first['month'] > 12) {
+                    $first['year']++;
+                    $first['month'] = 1;
+                }
+            } //while months
+ 
+            $add_day = 0;
+            while (smoothdate ($first['year'], $first['month'], $first['day'] + 1) <= $target) {
+                if (($first['year'] % 100 == 0) && ($first['year'] % 400 == 0)) {
+                    $month_lengths[1] = 29;	//leap year adjustment
+                } else {
+                    if ($first['year'] % 4 == 0) {
+                        $month_lengths[1] = 29; // leap year adjustment
+                    }
+                }
+                $add_day++;
+                $first['day']++;
+                if ($first['day'] > $month_lengths[$first['month'] - 1]) {
+                    $first['month']++;
+                    $first['day'] = 1;
+                    if ($first['month'] > 12) {
+                        $first['month'] = 1;
+                    }
+                }
+            } // while days
+ 
+            $retval = array ('years' => $add_year, 'months' => $add_month, 'days' => $add_day);
+        } else {
+			$retval = wfMsg('from-first',$first['year'], $first['month'], $first['day'], $second['year'], $second['month'], $second['day']);
+		}
+    }// validation ok
+ 
+    return $retval;
+}
---
+++ AgeParse.i18n.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * Internationalisation file for extension Age.
+ *
+ * @addtogroup Extensions
+*/
+$messages = array();
+ 
+/** English (default)
+ * @author Mark Daly
+*/
+$messages['en'] = array (
+	 'from'			 => 'from'
+	,'to'			 => 'to'
+	,'left'			 => 'left'
+	,'right'		 => 'right'
+	,'format'		 => 'format'
+	,'errbox'		 => 'errbox'
+	,'zeronegatives' => 'zeronegatives'
+	,'yes'			 => 'yes'
+	,'no'			 => 'no'
+	,'ymd'			 => 'ymd'
+	,'ym'			 => 'ym'
+	,'y'			 => 'y'
+	,'required-from' => 'FROM attribute is required (e.g. from="2009-01-12")'
+	,'required-to'   => 'TO attribute is  required (e.g. from="2009-01-12")'
+	,'invalid-from'  => 'FROM date is not valid: $1-$2-$3 (YYYY-MM-DD)'
+	,'invalid-to'    => 'TO date is not valid: $1-$2-$3 (YYYY-MM-DD)'
+	,'from-first'    => 'FROM ($1-$2-$3) date must occur before TO ($4-$5-$6) date'
+	,'same-day'      => 'FROM ($1) and TO ($2) date are the same'
+	,'syntax'        => '&lt;age from="yyyy-mm-dd" to="yyyy-mm-dd" [left="c"] [right="c"]&gt;&lt;/age&gt;'
+	,'catchall'      => 'Something is wrong with the AGE tag; check your syntax, match quotes, FROM & TO attributes are required, date format should be ISO-8601 (e.g. YYYY-MM-DD), and FROM must be less than TO.'
+	,'yy-single'     => 'year'
+	,'yy-plural'     => 'years'
+	,'mm-single'     => 'month'
+	,'mm-plural'     => 'months'
+	,'dd-single'     => 'day'
+	,'dd-plural'     => 'days'
+	,'answer-sep'    => ', '
+);
+ 
+/** Message documentation (explain purpose of each message)
+ * @author Mark Daly
+*/
+$messages['qqq'] = array (
+	 'from'			 => 'attribute to specify starting date'
+	,'to'			 => 'attribute to specify ending date'
+	,'left'			 => 'attribute to specify text added to front of answer'
+	,'right'		 => 'attribute to specify text added after answer'
+	,'format'		 => 'attribute to specify what parts of the answer you want to show'
+	,'errbox'		 => 'attribute to specify (yes/no) if error should show in a distinctive box'
+	,'zeronegatives' => 'attribute to specify (yes/no) if negative days for answer should just show as "0 days"'
+	,'yes'			 => 'answer used by errbox and zeronegatives'
+	,'no'			 => 'answer used by errbox and zeronegatives'
+	,'ymd'			 => 'format to show days, months, and years when that part is greater than zero'
+	,'ym'			 => 'format to show months and years when that part is greater than zero'
+	,'y'			 => 'format to only show years'
+	,'required-from' => 'error message: FROM value is required'
+	,'required-to'   => 'error message: TO value is required'
+	,'invalid-from'  => 'error message: FROM value is not a valid date'
+	,'invalid-to'    => 'error message: TO value is not a valid date'
+	,'from-first'    => 'error message: FROM date must be less than or equal to TO date'
+	,'same-day'      => 'error message: days are the same, why bother?'
+	,'syntax'        => 'error message: show how the tag can be written'
+	,'catchall'      => 'error message: show this when we know an error occurred but not what happened'
+	,'yy-single'     => 'word for 1 year'
+	,'yy-plural'     => 'word for many years'
+	,'mm-single'     => 'word for 1 day'
+	,'mm-plural'     => 'word for many months'
+	,'dd-single'     => 'word for 1 day'
+	,'dd-plural'     => 'word for many days'
+	,'answer-sep'    => 'separate year, month, and day values in answer'
+);
+ 
+/** Spanish (default)
+ * @author Google Translator (this is just for testing)
+*/
+$messages['es'] = array (
+	 'from'			 => 'desde'
+	,'to'			 => 'para'
+	,'left'			 => 'izquierda'
+	,'right'		 => 'acertado'
+	,'format'		 => 'formato'
+	,'errbox'		 => 'caja'
+	,'zeronegatives' => 'negativos'
+	,'yes'			 => 'si'
+	,'no'			 => 'no'
+	,'ymd'			 => 'ymd'
+	,'ym'			 => 'ym'
+	,'y'			 => 'y'
+	,'required-from' => 'FROM atributo es necesario (e.g. from="2009-01-12")'
+	,'required-to'   => 'TO atributo es necesario (e.g. from="2009-01-12")'
+	,'invalid-from'  => 'FROM fecha no es v‡lida: $1-$2-$3 (YYYY-MM-DD)'
+	,'invalid-to'    => 'TO fecha no es v‡lida: $1-$2-$3 (YYYY-MM-DD)'
+	,'from-first'    => 'FROM ($1-$2-$3) fecha debe ocurrir antes de TO ($4-$5-$6) fecha'
+	,'same-day'      => 'FROM ($1) y TO ($2) fecha son los mismos'
+	,'syntax'        => '&lt;age from="yyyy-mm-dd" to="yyyy-mm-dd" [left="c"] [right="c"]&gt;&lt;/age&gt;'
+	,'catchall'      => 'Algo est‡ mal con la edad etiqueta; comprobar su sintaxis, coinciden con las comillas, que van y vuelven a los atributos son necesarios, la fecha debe tener el formato ISO-8601 (por ejemplo, AAAA-MM-DD), y de debe ser inferior a A.'
+	,'yy-single'     => 'a–os'
+	,'yy-plural'     => 'a–os'
+	,'mm-single'     => 'mes'
+	,'mm-plural'     => 'mes'
+	,'dd-single'     => 'd’a'
+	,'dd-plural'     => 'd’a'
+	,'answer-sep'    => ', '
+);
+
+/** German
+ * @author DaSch
+*/
+$messages['de'] = array (
+	 'from'			 => 'von'
+	,'to'			 => 'bis'
+	,'left'			 => 'links'
+	,'right'		 => 'rechts'
+	,'format'		 => 'format'
+	,'errbox'		 => 'errbox'
+	,'zeronegatives' => 'zeronegatives'
+	,'yes'			 => 'ja'
+	,'no'			 => 'nein'
+	,'ymd'			 => 'ymd'
+	,'ym'			 => 'ym'
+	,'y'			 => 'y'
+	,'required-from' => 'FROM attribute is required (e.g. from="2009-01-12")'
+	,'required-to'   => 'TO attribute is  required (e.g. from="2009-01-12")'
+	,'invalid-from'  => 'FROM date is not valid: $1-$2-$3 (YYYY-MM-DD)'
+	,'invalid-to'    => 'TO date is not valid: $1-$2-$3 (YYYY-MM-DD)'
+	,'from-first'    => 'FROM ($1-$2-$3) date must occur before TO ($4-$5-$6) date'
+	,'same-day'      => 'FROM ($1) and TO ($2) date are the same'
+	,'syntax'        => '&lt;age from="yyyy-mm-dd" to="yyyy-mm-dd" [left="c"] [right="c"]&gt;&lt;/age&gt;'
+	,'catchall'      => 'Something is wrong with the AGE tag; check your syntax, match quotes, FROM & TO attributes are required, date format should be ISO-8601 (e.g. YYYY-MM-DD), and FROM must be less than TO.'
+	,'yy-single'     => 'Jahr'
+	,'yy-plural'     => 'Jahre'
+	,'mm-single'     => 'Monat'
+	,'mm-plural'     => 'Monate'
+	,'dd-single'     => 'Tag'
+	,'dd-plural'     => 'Tage'
+	,'answer-sep'    => ', '
+);
