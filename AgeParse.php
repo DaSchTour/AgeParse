b5db9400eead00bebc003e8e35d31b51a3c59632
DaSchTour <daschtour@me.com>
2012-07-15 17:59:20 +0200

    moved README added description message

--- AgeParse.php
+++ AgeParse.php
@@ -2,37 +2,6 @@
 /** AgeParse MediaWiki Extension
  * Type: parser
  *
- * This extension calculates the number of years plus months plus days between two ISO-8601 formatted
- * dates called FROM and TO. These values are specified as attributes to the {{#age}} function. All other
- * attributes are optional, incuding:
- *  from   = ISO-8601 (yyyy-mm-dd) starting date
- *  to     = ISO-8601 (uuuu-mm-dd) ending date
- *  format = controls how much of answer is displayed; choices: ymd | ym | y
- *  left   = character or text to add to front of answer
- *  right  = character or text to add to end of answer
- *  errbox = "yes" shows errors as class="errorbox"; "no" shows errors as regular text
- *  zeronegatives = "yes" shows "0 days" if TO occurs before FROM
- * 
- * == INSTALL ==
- *  1. Copy AGEPARSE.PHP and AGE.I18N.PHP into extensions/Age folder on wiki
- *  2. Add the following lines into the wiki LocalSettings.php
- *		 require_once( "$IP/extensions/AgeParse/AgeParse.php" );
- * 		 $wgAgeErrorBox      = true;  //true: show box if arguments/calculation cause error
- * 		 $wgAgeDefaultToday  = true;  //true: FROM or TO default to current date if not provided
- *		 $wgAgeZeroNegatives = true;  //true: show '0 days' if calculated value is negative
- *  3. Change globals to meet your needs
- *  4. Add {{#age: from=yyyy-mm-dd |to=yyy-mm-dd}} to page(s)
- *  
- * ==LANGUAGE ==
- * AgeParse.i18n.php available to implement translations
- * 
- * == SYNTAX ==
- * {{#age: from=yyyy-mm-dd | to=yyyy-mm-dd | left=C | right=C | format=ymd | errbox=yes | zeronegatives=yes }}
- *
- * == COMPATIBILITY ==
- * This extension cannot be 
- *  
- * == INFO ==
  * @author Mark Daly <research@chanur.com>
  * @version 0.1
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
@@ -47,7 +16,7 @@ if(!defined('MEDIAWIKI')){
 	die(-1);
 }
  
-DEFINE('AGEPARSE_VERSION','0.1 2009-03-13');
+DEFINE('AGEPARSE_VERSION','0.2 2009-03-13');
  
 /**
  * Identify the extension, version, author, etc
@@ -56,9 +25,10 @@ $wgExtensionCredits['parserhook'][] = array(
 		'path'          =>      __FILE__,
         'name'          =>      'AgeParse',
         'version'       =>      AGEPARSE_VERSION,
-        'author'        =>      'Mark Daly',
+        'author'        =>      'Mark Daly, [http://www.daschmedia.de DaSch]',
         'url'           =>      'http://www.mediawiki.org/wiki/Extension:AgeParse',
-        'description'   =>      'Calculate difference days, months, and years between two dates'
+        'description'   =>      'Calculate difference days, months, and years between two dates',
+        'descriptionmsg'=>      'ageparse-desc',
 );
  
  
